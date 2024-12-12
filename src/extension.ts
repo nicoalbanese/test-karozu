f94a1a2416b2979e736a8ed22256ab87348bed37
Nico Albanese <nico.albanese@vercel.com>
2024-12-12 13:44:37 +0000

    update

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,1 @@
-import { createCLI } from "karozu/cli"
-import { drizzle } from "./config";
-import { templates } from "./templates"
-
-export default createCLI(drizzle, templates)
+console.log("HELLo")
