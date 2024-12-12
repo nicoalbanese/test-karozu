5d80cde7e176e4cd0ba55ed1c7ccacd955ca83e4
Nico Albanese <nico.albanese@vercel.com>
2024-12-12 13:14:36 +0000

    update

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log("HELLO")
+import { drizzle } from "./config";
+import { templates } from "./templates";
+
+const compiled = drizzle.compile(templates, {
+  dbType: "mysql",
+  provider: "planetscale",
+});
+
+console.log(compiled);
