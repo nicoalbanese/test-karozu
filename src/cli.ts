e3ecb22abd2a44376b1dfab9ea5a929d7cc1bb75
Nico Albanese <nico.albanese@vercel.com>
2024-12-13 15:06:33 +0000

    update

---
+++ src/cli.ts
@@ -0,0 +1,22 @@
+import { CLI, Prompts } from "karozu/cli";
+import { drizzle } from "./config";
+import { templates } from "./templates";
+
+const prompts = new Prompts(drizzle, (p) => ({
+  dbType: () =>
+    p.select({
+      message: "Which DB type do you want to use?",
+      options: [{ value: "mysql" }, { value: "postgres" }, { value: "sqlite" }],
+    }),
+  provider: () =>
+    p.select({
+      message: "Which provider do you want to use?",
+      options: [
+        { value: "neon" },
+        { value: "turso" },
+        { value: "planetscale" },
+      ],
+    }),
+}));
+
+export default new CLI(drizzle, templates, prompts);
